03e1572d53de3a331418fe67766fa66805990322
sina <sina.nowroozi@gmail.com>
2023-02-16 16:39:08 -0500

    - working - imgui to play around with texture coordinates

--- main.cpp
+++ main.cpp
@@ -10,6 +10,47 @@
 
 #include "imgui_utils.h"
 
+class TextureCoordsUi {
+public:
+    float d_t_bottom_left_s = 0.0f;
+    float d_t_bottom_left_t = 1.0f;
+
+    float d_t_bottom_right_s = 1.0f;
+    float d_t_bottom_right_t = 1.0f;
+
+    float d_t_top_left_s = 1.0f;
+    float d_t_top_left_t = 0.5f;
+
+    float d_t_top_right_s = 0.0f;
+    float d_t_top_right_t = 0.5f;
+
+    float curr_t_bottom_left_s = d_t_bottom_left_s;
+    float curr_t_bottom_left_t = d_t_bottom_left_t;
+
+    float curr_t_bottom_right_s = d_t_bottom_right_s;
+    float curr_t_bottom_right_t = d_t_bottom_right_t;
+
+    float curr_t_top_left_s = d_t_top_left_s;
+    float curr_t_top_left_t = d_t_top_left_t;
+
+    float curr_t_top_right_s = d_t_top_right_s;
+    float curr_t_top_right_t = d_t_top_right_t;
+    
+    void default_vals() {
+        curr_t_bottom_left_s = d_t_bottom_left_s;
+        curr_t_bottom_left_t = d_t_bottom_left_t;
+
+        curr_t_bottom_right_s = d_t_bottom_right_s;
+        curr_t_bottom_right_t = d_t_bottom_right_t;
+
+        curr_t_top_left_s = d_t_top_left_s;
+        curr_t_top_left_t = d_t_top_left_t;
+
+        curr_t_top_right_s = d_t_top_right_s;
+        curr_t_top_right_t = d_t_top_right_t;
+    }
+};
+
 void lab1() {
     glBegin(GL_LINES);
 
@@ -42,34 +83,34 @@ void lab2() {
     glFlush();
 }
 
-void lab3() {
+void lab3(TextureCoordsUi* ui_state) {
     // Start the Dear ImGui frame
     imgui_new_frame();
     bool show_demo_window = true;
 
-    float d_t_bottom_left_s = 0.0f;
-    float d_t_bottom_left_t = 1.0f;
+//    float d_t_bottom_left_s = 0.0f;
+//    float d_t_bottom_left_t = 1.0f;
+//
+//    float d_t_bottom_right_s = 1.0f;
+//    float d_t_bottom_right_t = 1.0f;
+//
+//    float d_t_top_left_s = 1.0f;
+//    float d_t_top_left_t = 0.5f;
+//
+//    float d_t_top_right_s = 0.0f;
+//    float d_t_top_right_t = 0.5f;
 
-    float d_t_bottom_right_s = 1.0f;
-    float d_t_bottom_right_t = 1.0f;
-
-    float d_t_top_left_s = 1.0f;
-    float d_t_top_left_t = 0.5f;
-
-    float d_t_top_right_s = 0.0f;
-    float d_t_top_right_t = 0.5f;
-
-    float curr_t_bottom_left_s = d_t_bottom_left_s;
-    float curr_t_bottom_left_t = d_t_bottom_left_t;
-
-    float curr_t_bottom_right_s = d_t_bottom_right_s;
-    float curr_t_bottom_right_t = d_t_bottom_right_t;
-
-    float curr_t_top_left_s = d_t_top_left_s;
-    float curr_t_top_left_t = d_t_top_left_t;
-
-    float curr_t_top_right_s = d_t_top_right_s;
-    float curr_t_top_right_t = d_t_top_right_t;
+//    float curr_t_bottom_left_s = d_t_bottom_left_s;
+//    float curr_t_bottom_left_t = d_t_bottom_left_t;
+//
+//    float curr_t_bottom_right_s = d_t_bottom_right_s;
+//    float curr_t_bottom_right_t = d_t_bottom_right_t;
+//
+//    float curr_t_top_left_s = d_t_top_left_s;
+//    float curr_t_top_left_t = d_t_top_left_t;
+//
+//    float curr_t_top_right_s = d_t_top_right_s;
+//    float curr_t_top_right_t = d_t_top_right_t;
 
 
     static bool no_menu = false;
@@ -98,53 +139,63 @@ void lab3() {
     if (ImGui::CollapsingHeader("Texture Coordinates")) {
         ImGui::Text("Values (s,t):");
         char t_bot_left_s[64] = "";
-        sprintf(t_bot_left_s, "%.2f", curr_t_bottom_left_s);
+        sprintf(t_bot_left_s, "%.2f", ui_state->curr_t_bottom_left_s);
         ImGui::InputText("bottom left s", t_bot_left_s, 64, ImGuiInputTextFlags_CharsDecimal);
 
         static char t_bot_left_t[64] = "";
-        sprintf(t_bot_left_t, "%.2f", curr_t_bottom_left_t);
+        sprintf(t_bot_left_t, "%.2f", ui_state->curr_t_bottom_left_t);
         ImGui::InputText("bottom left t", t_bot_left_t, 64, ImGuiInputTextFlags_CharsDecimal);
 
         ImGui::Spacing();
 
         static char t_bot_right_s[64] = "";
-        sprintf(t_bot_right_s, "%.2f", curr_t_bottom_right_s);
+        sprintf(t_bot_right_s, "%.2f", ui_state->curr_t_bottom_right_s);
         ImGui::InputText("bottom right s", t_bot_right_s, 64, ImGuiInputTextFlags_CharsDecimal);
 
         static char t_bot_right_t[64] = "";
-        sprintf(t_bot_right_t, "%.2f", curr_t_bottom_right_t);
+        sprintf(t_bot_right_t, "%.2f", ui_state->curr_t_bottom_right_t);
         ImGui::InputText("bottom right t", t_bot_right_t, 64, ImGuiInputTextFlags_CharsDecimal);
 
         ImGui::Spacing();
 
         static char t_top_left_s[64] = "";
-        sprintf(t_top_left_s, "%.2f", curr_t_top_left_s);
+        sprintf(t_top_left_s, "%.2f", ui_state->curr_t_top_left_s);
         ImGui::InputText("top left s", t_top_left_s, 64, ImGuiInputTextFlags_CharsDecimal);
 
         static char t_top_left_t[64] = "";
-        sprintf(t_top_left_t, "%.2f", curr_t_top_left_t);
+        sprintf(t_top_left_t, "%.2f", ui_state->curr_t_top_left_t);
         ImGui::InputText("top left t", t_top_left_t, 64, ImGuiInputTextFlags_CharsDecimal);
 
         ImGui::Spacing();
         static char t_top_right_s[64] = "";
-        sprintf(t_top_right_s, "%.2f", curr_t_top_right_s);
+        sprintf(t_top_right_s, "%.2f", ui_state->curr_t_top_right_s);
         ImGui::InputText("top right s", t_top_right_s, 64, ImGuiInputTextFlags_CharsDecimal);
 
         static char t_top_right_t[64] = "";
-        sprintf(t_top_right_t, "%.2f", curr_t_top_right_t);
+        sprintf(t_top_right_t, "%.2f", ui_state->curr_t_top_right_t);
         ImGui::InputText("top right t", t_top_right_t, 64, ImGuiInputTextFlags_CharsDecimal);
 
-        curr_t_bottom_left_s = atof(t_bot_left_s);
-        curr_t_bottom_left_t = atof(t_bot_left_t);
+        ui_state->curr_t_bottom_left_s = atof(t_bot_left_s);
+        ui_state->curr_t_bottom_left_t = atof(t_bot_left_t);
 
-        curr_t_bottom_right_s = atof(t_bot_right_s);
-        curr_t_bottom_right_t = atof(t_bot_right_t);
+        ui_state->curr_t_bottom_right_s = atof(t_bot_right_s);
+        ui_state->curr_t_bottom_right_t = atof(t_bot_right_t);
 
-        curr_t_top_left_s = atof(t_top_left_s);
-        curr_t_top_left_t = atof(t_top_left_t);
+        ui_state->curr_t_top_left_s = atof(t_top_left_s);
+        ui_state->curr_t_top_left_t = atof(t_top_left_t);
 
-        curr_t_top_right_s = atof(t_top_right_s);
-        curr_t_top_right_t = atof(t_top_right_t);
+        ui_state->curr_t_top_right_s = atof(t_top_right_s);
+        ui_state->curr_t_top_right_t = atof(t_top_right_t);
+
+
+        bool reset = false;
+        reset |= ImGui::Button("Reset");
+        if (reset)
+        {
+            ui_state->default_vals();
+            std::cout << "hello " << ui_state->curr_t_bottom_left_s << std::endl;
+
+        }
     }
 
     ImGui::PopItemWidth();
@@ -157,16 +208,16 @@ void lab3() {
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
     glBegin(GL_QUADS);
 //    glTexCoord2f(0.0f, 1.0f);
-    glTexCoord2f(curr_t_bottom_left_s, curr_t_bottom_left_t);
+    glTexCoord2f(ui_state->curr_t_bottom_left_s, ui_state->curr_t_bottom_left_t);
     glVertex3f(-.5f, -.5f, 1.0f);
 
-    glTexCoord2f(curr_t_bottom_right_s, curr_t_bottom_right_t);
+    glTexCoord2f(ui_state->curr_t_bottom_right_s, ui_state->curr_t_bottom_right_t);
     glVertex3f(.5f, -.5f, 1.0f);
 
-    glTexCoord2f(curr_t_top_left_s, curr_t_top_left_t);
+    glTexCoord2f(ui_state->curr_t_top_left_s, ui_state->curr_t_top_left_t);
     glVertex3f(.5f, .5f, 1.0f);
 
-    glTexCoord2f(curr_t_top_right_s, curr_t_top_right_t);
+    glTexCoord2f(ui_state->curr_t_top_right_s, ui_state->curr_t_top_right_t);
     glVertex3f(-.5f, .5f, 1.0f);
     glEnd();
 }
@@ -235,6 +286,19 @@ int main(int argc, char** argv) {
     bool show_demo_window = true;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
+    TextureCoordsUi ui_state;
+    ui_state.d_t_bottom_left_s = 0.0f;
+    ui_state.d_t_bottom_left_t = 1.0f;
+
+    ui_state.d_t_bottom_right_s = 1.0f;
+    ui_state.d_t_bottom_right_t = 1.0f;
+
+    ui_state.d_t_top_left_s = 1.0f;
+    ui_state.d_t_top_left_t = 0.5f;
+
+    ui_state.d_t_top_right_s = 0.0f;
+    ui_state.d_t_top_right_t = 0.5f;
+
     // Main loop
     while (!glfwWindowShouldClose(window)) {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -242,7 +306,8 @@ int main(int argc, char** argv) {
 
 
 //        lab2();
-        lab3();
+
+        lab3(&ui_state);
 //        lab99();
 
         glfwSwapBuffers(window);
